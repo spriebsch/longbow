12bfb287a12ae2df2c12dabb9debcf7568caa115
Stefan Priebsch <stefan@priebsch.de>
2025-03-15 23:30:55 +0100

    Documentation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Longbow
 
-Event dispatching and handling system.
+Aim high and shoot far with Longbow, the PHP event framework.
 
 (c) by Stefan Priebsch.
--- format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./add-copyright-notice.php
-~/Software/phpstorm/bin/phpstorm.sh format -r src/ -s CodingStyle.xml
-~/Software/phpstorm/bin/phpstorm.sh format -r tests/ -s CodingStyle.xml
